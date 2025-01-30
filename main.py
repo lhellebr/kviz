309fa5768161a19694922d75a8a00db14b34cc0f
Lukas Hellebrandt <lhellebr@redhat.com>
2025-01-30 14:06:09 +0100

    Point counting

--- main.py
+++ main.py
@@ -29,15 +29,33 @@ class Kviz(tk.Frame):
             'home': HomeFrame(self, categories),
             'question': QuestionFrame(self),
         }
-        VolumeControls(self, self.font_resize).pack(fill=tk.X)
+        header = tk.Frame(self)
+        header.pack(fill=tk.X)
+        header.configure(background=COLOR, pady="2", padx="2") 
+        VolumeControls(header, self.font_resize).pack(fill=tk.X, side="right")
+        self.pointLabel = tk.Label(header, text=0)
+        self.pointLabel.pack(fill=tk.X, side="left")
         self.current_frame = 'home'
         self.frames['home'].pack(expand=True, fill=tk.BOTH)
         self.bind('<Configure>', self.font_resize)
+        self.points = 0
 
     def switch_frames(self, frame):
         self.frames[self.current_frame].pack_forget()
         self.frames[frame].pack(expand=True, fill=tk.BOTH)
         self.current_frame = frame
+
+    def correct(self):
+        self.points = self.points + 1
+        self.pointLabel.configure(text=self.points)
+        self.switch_frames('home')
+
+    def wrong(self):
+        self.switch_frames('home')
+
+    def ask_question(self, item):
+        self.frames['question'].ask(item)
+        self.switch_frames('question')
 
     def font_resize(self, event):
         if type(event) == str:
@@ -55,20 +73,33 @@ class HomeFrame(tk.Frame):
         for category_number, category in enumerate(categories):
             tk.Label(self, text=category.name).grid(column=category_number, row=0, sticky="news")
             for item_number, item in enumerate(category.items):
-                tk.Button(self, text=item.question, command=lambda: parent.switch_frames('question')).grid(column=category_number, row=item_number+1, sticky="news")
+                button = tk.Button(self, text=item.question)
+                button.grid(column=category_number, row=item_number+1, sticky="news")
+                button.configure(command = lambda button_fixed=button: self.question_clicked(button_fixed, item))
         self.columnconfigure(tuple(range(self.grid_size()[0])), weight=1)
         self.rowconfigure(tuple(range(1, self.grid_size()[1])), weight=1)
         
+    def question_clicked(self, button, item):
+        button.grid_forget()
+        button.master.master.ask_question(item)
 
 class QuestionFrame(tk.Frame):
     def __init__(self, parent):
         super().__init__(parent)
-        tk.Button(self, text="+", command=lambda: parent.switch_frames('home')).pack()
+        tk.Button(self, text="<-", command=lambda: parent.switch_frames('home')).pack()
+
+    def ask(self, item):
+        for widget in self.winfo_children():
+            widget.destroy()
+        tk.Label(self, text=item.question).pack()
+        answerFrame = tk.Frame(self).pack()
+        for option in item.options:
+            tk.Button(self, text=option.text, command=self.master.correct if option.correct else self.master.wrong).pack()
+
 
 class VolumeControls(tk.Frame):
     def __init__(self, parent, method):
         super().__init__(parent)
-        self.configure(background=COLOR, pady="2") 
         self.method = method
         tk.Button(self, height=-1, font=tkFont.nametofont('TkTextFont'), text="+", command=lambda: method("+")).pack(side=tk.RIGHT)
         tk.Button(self, height=-1, font=tkFont.nametofont('TkTextFont'), text="-", command=lambda: method("-")).pack(side=tk.RIGHT)
